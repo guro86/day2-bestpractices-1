12b08479ba0c62c83c9e8f8a5e7e4cd6ebd48d20
Gustav Robertson <gustav.robertson@gmail.com>
2022-02-22 13:20:45 +0100

    Updated with the initial example

---
+++ test_animals.py
@@ -0,0 +1,16 @@
+# Import classes from your brand new package
+from animals import Mammals
+from animals import Birds
+# Create an object of Mammals class & call a method of it
+myMammal = Mammals()
+myMammal.printMembers()
+# Create an object of Birds class & call a method of it
+myBird = Birds()
+myBird.printMembers()
+
+
+#harmless_birds = animals.harmless.Birds()
+#harmless_birds.printMembers()
+#
+#dangerous_fish = animals.dangerous.Fish()
+#dangerous_fish.printMembers()
